c4696b00cd714ac3e634c6b2bb7becdb593ab67e
Mari0w0 <marilynperalta002@gmail.com>
2021-06-13 19:59:14 -0400

    adds user questions and characters

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowercasechar = "abcdefghijklmnopqrstuvwxyz"
+var uppercasechar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = "0123456789"
+//add special characters
+var generatePassword = function(){
+  var passwordlength = window.prompt("how long is your password?")
+  passwordlength = Number(passwordlength)
+  console.log (passwordlength)
+  var isuppercase = window.confirm("would you like uppercase letters?")
+  var islowercase = window.confirm("would you like lowercase letters?")
+  var isnums = window.confirm("would you like numbers?")
+  var isspecial = window.confirm("would you like special characters?")
+  console.log(isuppercase, islowercase, isnums, isspecial)
+  
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
